c68e73dc0f1bc35b4185588b8d8b87e6c0dee508
gerritmoore8 <gerritmoore42@gmail.com>
2020-11-18 20:02:13 -0700

    recent changes

--- IR/IR.ino
+++ IR/IR.ino
@@ -1,12 +1,12 @@
 int emitterPin = 3;
-int receiverPin = A0;
+int topPin = A0;
 int bottomPin = A1;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(emitterPin, OUTPUT);
   pinMode(emitterPin + 1, OUTPUT);
-  pinMode(receiverPin, INPUT);
+  pinMode(topPin, INPUT);
   pinMode(bottomPin, INPUT);
   Serial.begin(9600);
 }
@@ -14,8 +14,8 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   static float data = 0;
-  digitalWrite(emitterPin, HIGH);
-  digitalWrite(emitterPin + 1, HIGH);
+ // digitalWrite(emitterPin, HIGH);
+  //digitalWrite(emitterPin + 1, HIGH);
 
   Serial.print("Top = ");
   Serial.println(analogRead(receiverPin));
